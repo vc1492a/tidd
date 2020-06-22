a41ed8fda7179701ca86cc3883e7c29cf408ea95
vc1492a <vc1492a@gmail.com>
2020-06-21 20:47:03 -0700

    version number bump

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup
 setup(
     name='src',
     packages=[''],
-    version='0.0.1',
+    version='0.0.2',
     description='',
     author='Valentino Constantinou',
     author_email='vconstan@jpl.nasa.gov',
     url='https://github.com/vc1492a/sTEC-d-dt-Anomaly-Detection',
-    download_url='https://github.com/vc1492a/sTEC-d-dt-Anomaly-Detection/archive/0.0.1.tar.gz',
+    download_url='https://github.com/vc1492a/sTEC-d-dt-Anomaly-Detection/archive/0.0.2.tar.gz',
     keywords=['tsunami', 'anomaly', 'detection'],
     classifiers=[],
     license='Apache License, Version 2.0',
     install_requires=[
-        'pandas'
+        'pandas',
+        'scikit-learn',
+        'tqdm'
     ]
 )
