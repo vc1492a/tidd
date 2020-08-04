4d544530b5d69bd6f1e0ec1097a6edd727cb08bd
Hamlin Liu <hamlin.liu@gmail.com>
2020-08-03 21:33:59 -0700

    Improved unit tests from 6 mins to 39 secs, Created function to generate random data in unit test

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pytest
 import random
 from src.data import read_day, normalize
+import os
 
 
 @pytest.fixture()
@@ -23,48 +24,75 @@ def test_dataframe() -> 'pd.DataFrame':
 
     return df
 
+@pytest.fixture
+def test_random_data() -> 'str':
+    """
+    fixture that creates the random data for testing
+    with read_data
+    :return: the file paths of the random data
+    """
+    path = './data/test'
+    year_path = path + '/2012'
+    day_path = year_path + '/300'
+    os.mkdir(path)
+    os.mkdir(year_path)
+    os.mkdir(day_path)
+    file_path =  day_path + '/test3000.12o_G01.txt'
+    with open(file_path, 'w') as test_file:
+        index_row = 'sod\t\tdsTEC/dt\t\tlon\t\tlat\t\th_ipp\t\telev\t\tazi\n'
+        test_data_row = '8250.0\t\t-0.01919536491673681\t\t-165.3638966481876\t\t24.961242295471187\t\t349982.9213181995\t\t10.1815617588652\t\t302.5328897665781\n'
+        test_file.write(index_row)
+        test_file.write(test_data_row)
+    return file_path
 
-def test_read_data_day() -> None:
+
+def test_read_data_day(test_random_data) -> None:
     """
     Tests whether the read_day function reads the appropriate year and day
     of year of data, and that it returns the data in the appropriate format.
     :return: None
     """
 
-    settings = [
-        tuple((2012, 303, "hawaii")),
-        tuple((2015, 259, "chile"))
-    ]
+    file_path = test_random_data.split('/') 
 
-    for s in settings:
+    s = tuple((int(file_path[3]), int(file_path[4]), file_path[2]))
 
-        # try to read data from a specific location
-        df = read_day(
-            location=s[2],
-            year=s[0],
-            day_of_year=s[1]
-        )
+    # try to read data from a specific location
+    df = read_day(
+        location=s[2],
+        year=s[0],
+        day_of_year=s[1]
+    )
 
-        # check to ensure that the returned data is a dataframe
-        assert isinstance(df, pd.DataFrame)
+    # check to ensure that the returned data is a dataframe
+    assert isinstance(df, pd.DataFrame)
 
-        # check to ensure that the index is a Pandas DateTimeIndex
-        assert type(df.index).__name__ == "DatetimeIndex"
+    # check to ensure that the index is a Pandas DateTimeIndex
+    assert type(df.index).__name__ == "DatetimeIndex"
 
-        # check that the day corresponds to the correct day of year
-        random_date = random.choice(pd.to_datetime(df.index.values))
-        assert random_date.dayofyear == s[1]
-        assert random_date.year == s[0]
+    # check that the day corresponds to the correct day of year
+    random_date = random.choice(pd.to_datetime(df.index.values))
+    assert random_date.dayofyear == s[1]
+    assert random_date.year == s[0]
 
-        # check that the column count is the same across data types
-        cols = ["_lat", "_lon", "_h_ipp", "_ele", "_azi"]
-        counts = list()
-        columns = list(df.columns.values)
-        for c in cols:
-            type_cols = [col for col in columns if c in col]
-            counts.append(len(type_cols))
+    # check that the column count is the same across data types
+    cols = ["_lat", "_lon", "_h_ipp", "_ele", "_azi"]
+    counts = list()
+    columns = list(df.columns.values)
+    for c in cols:
+        type_cols = [col for col in columns if c in col]
+        counts.append(len(type_cols))
 
-        assert all(x == counts[0] for x in counts)
+    assert all(x == counts[0] for x in counts)
+
+    # deleting the test files
+    os.remove(test_random_data)
+    # deleting the directories
+    os.rmdir('/'.join(file_path[:-1]))
+    os.rmdir('/'.join(file_path[:-2]))
+    os.rmdir('/'.join(file_path[:-3]))
+
+
 
 
 def test_normalize_data(test_dataframe) -> None:
